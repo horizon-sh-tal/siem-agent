287632f30f3ce8abc6b8a902f4ab863b95af8623
thisisdhruvchopra <thisisdhruvchopra@gmail.com>
2026-02-18 16:57:07 +0530

    pushpa3

--- ftp-server/add_ftp_users.sh
+++ ftp-server/add_ftp_users.sh
@@ -3,13 +3,13 @@ set -e
 
 # Only add privileged users (prof1, prof2)
 pure-pw useradd prof1 -u ftpuser -d /home/ftpusers/prof1 <<EOF
-[prof1_password]
-[prof1_password]
+Maharanapratap!
+Maharanapratap!
 EOF
 
 pure-pw useradd prof2 -u ftpuser -d /home/ftpusers/prof2 <<EOF
-[prof2_password]
-[prof2_password]
+gogreen@7560
+gogreen@7560
 EOF
 
 pure-pw mkdb
